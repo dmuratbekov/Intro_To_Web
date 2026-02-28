80adf5070e05d0e95e88ce93d3357978d9fa35f9
md12370@auca.kg <101464416+dmuratbekov@users.noreply.github.com>
2026-02-28 15:11:46 +0600

    labs 26-34

---
+++ lab34/ex1.js
@@ -0,0 +1,9 @@
+let a = 11
+let b = 4
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);
